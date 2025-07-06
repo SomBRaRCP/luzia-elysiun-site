ad3b66457b4174c359a058182cab501f46327d9d
Reginaldo C Pires <132711893+SomBRaRCP@users.noreply.github.com>
2025-07-05 21:27:44 -0300

    Update luzia.js

--- docs/js/luzia.js
+++ docs/js/luzia.js
@@ -1,4 +1,4 @@
-const luziaAPI = "https://5a5d-2804-7f4-3d42-75f3-1127-9100-65f5-4944.ngrok-free.app/conversar";
+const luziaAPI = "https://7ed2-2804-7f4-3d42-75f3-1127-9100-65f5-4944.ngrok-free.app/conversar";
 
 document.getElementById("enviar").addEventListener("click", async () => {
     const entrada = document.getElementById("entrada");
