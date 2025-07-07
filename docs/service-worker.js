0405ed28144dee9eeb8d2e04f6a38d48ea411d69
Reginaldo C Pires <132711893+SomBRaRCP@users.noreply.github.com>
2025-07-06 22:11:13 -0300

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,20 +1,21 @@
-// Simples: cache apenas os arquivos estáticos essenciais
-const CACHE = "luzia-v1";
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./css/estilo.css",
-        "./js/luzia.js",
-        "./assets/logo.png"
-      ])
-    )
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('luzia-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './css/estilo.css',
+        './js/luzia.js',
+        './assets/logo.png'
+      ]);
+    })
   );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
